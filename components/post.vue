b5632c14467d439901e5b632a8bfc6679fcf3744
umudik <seyitumutbicer@gmail.com>
2021-06-09 01:01:17 +0300

    dev

---
+++ components/post.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>{{ model }}</div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    props: {
+        model: {
+            type: String,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            model: null,
+        };
+    },
+    created: async function () {
+      // this.model = await axois.options(`${this.fookie.baseURL}/${model}`);
+    },
+};
+</script>
+
+<style>
+</style>
--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-import get from './components/get'
-
 export default {
-    install: (app, options) => {
-        app.provide('fookie', options)
-
-        app.directive('my-directive', {
-            mounted(el, binding, vnode, oldVnode) {
-                // some logic ...
+    install(Vue, options) {
+        Vue.mixin({
+            mounted() {
+                console.log('Mounted!');
             }
-
-        })
-
-        app.components({
-            "fookie-get": get
-        })
-
-        app.mixin({
-            created() {
-                // some logic ...
-            }
-
-        })
+        });
     }
-}
+};
